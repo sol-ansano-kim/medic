56cc0944778a5250d3a92a055129802726b25bd2
Sol Kim <defreturnnone@gmail.com>
2017-02-24 13:49:14 +0900

    add testNodes visitor

---
+++ python/medic/visitors/testNodes.py
@@ -0,0 +1,19 @@
+from ..core import visitor
+from ..core import nodes
+
+
+class TestNodes(visitor.Visitor):
+    def __init__(self):
+        super(TestNodes, self).__init__()
+
+    def visit(self, karte):
+        karte.resetResults()
+
+        all_nodes = nodes.GetNodes()
+
+        for tester in karte.testers():
+            test_nodes = filter(lambda x : tester.Match(x.object()), all_nodes)
+
+            for node in test_nodes:
+                if tester.Test(node):
+                    karte.addResult(tester, node)
