24a669ab941033c6c9f41a429fc40bcda98d14fd
Sol Kim <defreturnnone@gmail.com>
2017-02-24 14:21:44 +0900

    add referenced tester

---
+++ python/medic/plugins/Tester/referenced.py
@@ -0,0 +1,17 @@
+from medic.core import testerBase
+
+
+class Referenced(testerBase.TesterBase):
+    Name = "Referenced"
+
+    def __init__(self):
+        super(Referenced, self).__init__()
+
+    def Match(self, m_object):
+        return True
+
+    def Test(self, node):
+        return node.dg().isFromReferencedFile()
+
+
+Tester = Referenced
