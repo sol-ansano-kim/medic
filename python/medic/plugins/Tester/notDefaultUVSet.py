0bd3e587148beb5c8b01c251bbeadbc3247ca96e
Sol Kim <defreturnnone@gmail.com>
2017-02-25 20:32:27 +0900

    add notDefaultUVSet tester

---
+++ python/medic/plugins/Tester/notDefaultUVSet.py
@@ -0,0 +1,25 @@
+from medic.core import testerBase
+from maya import OpenMaya
+
+
+class NotDefaultUVSet(testerBase.TesterBase):
+    Name = "NotDefaultUVSet"
+    __DefaultSetName = "map1"
+
+    def __init__(self):
+        super(NotDefaultUVSet, self).__init__()
+
+    def Match(self, node):
+        return node.object().hasFn(OpenMaya.MFn.kMesh)
+
+    def Test(self, node):
+        mesh = None
+        try:
+            mesh = OpenMaya.MFnMesh(node.object())
+        except:
+            return False
+
+        return mesh.currentUVSetName() != NotDefaultUVSet.__DefaultSetName
+
+
+Tester = NotDefaultUVSet
