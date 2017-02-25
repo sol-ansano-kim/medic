a423895872446b62410f828d2ec1132306fb79cb
Sol Kim <defreturnnone@gmail.com>
2017-02-25 20:33:13 +0900

    add forceReload option to GetKarte() function

--- python/medic/function/__init__.py
+++ python/medic/function/__init__.py
@@ -5,8 +5,8 @@ def GetKartes(forceReload=False):
     return collector.GetKartes(forceReload=forceReload)
 
 
-def GetKarte(karte_name):
-    kartes = collector.GetKartes()
+def GetKarte(karte_name, forceReload=False):
+    kartes = collector.GetKartes(forceReload=forceReload)
     for karte in kartes:
         if karte.name() == karte_name:
             return karte
