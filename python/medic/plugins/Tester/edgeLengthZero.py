6a5472e06e3b03ab8f4988b50aae90a30643e985
Sol Kim <defreturnnone@gmail.com>
2017-02-27 10:55:15 +0900

    add edgeLengthZero tester

---
+++ python/medic/plugins/Tester/edgeLengthZero.py
@@ -0,0 +1,37 @@
+from medic.core import testerBase
+from maya import OpenMaya
+import sys
+
+
+class EdgeLengthZero(testerBase.TesterBase):
+    Name = "EdgeLengthZero"
+    ThreadsHold = 0.001
+
+    def __init__(self):
+        super(EdgeLengthZero, self).__init__()
+
+    def Match(self, node):
+        return node.object().hasFn(OpenMaya.MFn.kMesh)
+
+    def Test(self, node):
+        if node.object().hasFn(OpenMaya.MFn.kMesh):
+            it = None
+            try:
+                it = OpenMaya.MItMeshEdge(node.object())
+            except:
+                return False
+
+            su = OpenMaya.MScriptUtil()
+            dp = su.asDoublePtr()
+
+            while (not it.isDone()):
+                it.getLength(dp)
+                if EdgeLengthZero.ThreadsHold > OpenMaya.MScriptUtil_getDouble(dp):
+                    return True
+
+                it.next()
+
+        return False
+
+
+Tester = EdgeLengthZero
