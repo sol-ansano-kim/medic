cd609cc25e6cb9eda65f8ce0f64221dea4483d15
Sol Kim <defreturnnone@gmail.com>
2017-12-16 01:15:36 +0900

    add debug.h

---
+++ include/medic/debug.h
@@ -0,0 +1,20 @@
+#ifndef MEDIC_DEBUG_INCLUDED
+#define MEDIC_DEBUG_INCLUDED
+
+#include <string.h>
+
+#ifdef _DEBUG
+#include <iostream>
+#endif
+
+
+namespace MEDIC
+{
+    void warn(std::string text);
+    void debug(std::string text);
+    void error(std::string text);
+}
+
+
+
+#endif // MEDIC_DEBUG_INCLUDED
