068abedcfad535ecabafe72068f2f9886b691f19
Gaetan Guidet <guidet@marza.com>
2019-06-18 11:45:27 +0900

    add optional 'parent' keyword argument to medicUI.Show function

--- python/medicUI/__init__.py
+++ python/medicUI/__init__.py
@@ -7,13 +7,16 @@ from maya import cmds
 MedicUIInstance = None
 
 
-def Show(visitorClass=None, karteName=None, showAllKartes=False):
+def Show(visitorClass=None, karteName=None, showAllKartes=False, parent=None):
     global MedicUIInstance
 
     if cmds.window(window.MainWindow.Name, q=True, ex=True):
         cmds.deleteUI(window.MainWindow.Name)
 
-    MedicUIInstance = window.MainWindow(visitorClass=visitorClass, parent=functions.getMayaMainWindow())
+    if parent is None:
+        parent = functions.getMayaMainWindow()
+
+    MedicUIInstance = window.MainWindow(visitorClass=visitorClass, parent=parent)
     MedicUIInstance.show()
 
     MedicUIInstance.showAllKartes(showAllKartes)
